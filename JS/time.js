0b19827cd6e048fdb1b818e37bf230bf4545af17
mqteorrr <93096355+mqteorrr@users.noreply.github.com>
2021-10-30 23:19:39 +0200

    Update time.js

--- JS/time.js
+++ JS/time.js
@@ -3,6 +3,6 @@ document.getElementById("datetime").innerHTML = dt.toLocaleString();
 setTimeout(() => {  newTime(); }, 500);
 function newTime() {
   var dt = new Date();
-  document.getElementById("datetime").innerHTML = dt.toLocateString();
+  document.getElementById("datetime").innerHTML = dt.toLocaleString();
   setTimeout(() => {  newTime(); }, 500);
 }
